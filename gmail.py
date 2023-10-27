d104988e976d19f314285c85dda13d951e5455db
Micah Beech <47065407+micahbeech@users.noreply.github.com>
2023-10-27 02:42:17 -0400

    Add emails

---
+++ gmail.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+from base64 import urlsafe_b64encode
+from email.message import EmailMessage
+import mimetypes
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# Ref:
+#   - https://developers.google.com/gmail/api/quickstart/python#set_up_your_environment
+#   - https://developers.google.com/gmail/api/guides/sending#python
+
+# If modifying these scopes, delete the token file.
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.send',
+]
+
+SCRIPT_DIR = Path(__file__).parent.resolve()
+TOKEN_FILE = SCRIPT_DIR / 'gmail-token.json'
+CREDENTIAL_FILE = SCRIPT_DIR / 'gmail-credentials.json'
+
+def getCredentials() -> Credentials:
+    '''
+    Fetch, refresh, or generate OAuth credentials for gmail.
+
+    If gmail-token.json contains credentials, they are returned or refreshed if needed.
+    Otherwise, new credentials will be generated based on gmail-credentials.json and saved (requires a browser).
+    '''
+    credentials = None
+
+    if TOKEN_FILE.is_file():
+        credentials = Credentials.from_authorized_user_file(TOKEN_FILE.resolve(), SCOPES)
+
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIAL_FILE, SCOPES)
+            credentials = flow.run_local_server(port=0, authorization_prompt_message=None)
+        
+        with open(TOKEN_FILE, 'w') as token:
+            token.write(credentials.to_json())
+
+    return credentials
+
+def sendEmail(to: str, subject: str, body: str, bodyType: str = None, attachments: list = []):
+    '''
+    Sends an email to the specified address with subject and body from the authorized email address.
+    '''
+    service = build('gmail', 'v1', credentials=getCredentials())
+    user = service.users().getProfile(userId='me').execute()
+
+    message = EmailMessage()
+
+    if bodyType:
+        message.set_content(body, bodyType)
+    else:
+        message.set_content(body)
+
+    message['To'] = to
+    message['From'] = user['emailAddress']
+    message['Subject'] = subject
+
+    for attachment in attachments:
+        type_subtype, _ = mimetypes.guess_type(attachment)
+        maintype, subtype = type_subtype.split('/')
+
+        with open(attachment, 'rb') as fp:
+            attachment_data = fp.read()
+        
+        message.add_attachment(attachment_data, maintype, subtype)
+
+    encoded_message = urlsafe_b64encode(message.as_bytes()).decode()
+    create_message = {'raw': encoded_message}
+    service.users().messages().send(userId='me', body=create_message).execute()
+
+
+if __name__ == '__main__':
+    print('Refreshing gmail credentials if needed...')
+    getCredentials()
+    print(f'Done. See {TOKEN_FILE}')
--- speedtest.py
+++ speedtest.py
@@ -6,14 +6,18 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from argparse import ArgumentParser
 import csv
+from dataclasses import dataclass
+from datetime import datetime
+from gmail import sendEmail
 
-RESULTS_PATH = Path(__file__).parent / 'results.csv'
+SCRIPT_PATH = Path(__file__).parent
+RESULTS_CSV_PATH = SCRIPT_PATH / 'results.csv'
 BPS_IN_MBPS = 125000
 
 def exec(command: str) -> str:
     return subprocess.run(command, shell=True, check=True, capture_output=True, text=True).stdout
 
-def runSpeedtest():
+def runSpeedtest(filename: Path):
     response = exec('speedtest --accept-license --accept-gdpr --format json')
     data = json.loads(response)
 
@@ -24,9 +28,9 @@ def runSpeedtest():
         'ping': data['ping']['latency'],
     }
     
-    header = None if RESULTS_PATH.is_file() else results.keys()
+    header = None if filename.is_file() else results.keys()
 
-    with open(RESULTS_PATH.resolve(), 'a') as file:
+    with open(filename, 'a') as file:
         writer = csv.writer(file)
 
         if header:
@@ -34,8 +38,9 @@ def runSpeedtest():
         
         writer.writerow(results.values())
 
-def generateGraphs(data: pd.DataFrame):
+def generateGraphs(data: pd.DataFrame) -> Path:
     axes = data.plot.line(x='time', subplots=[('down', 'up')], figsize=(6,8), grid=True)
+
     axes[0].set_ylabel('Bandwidth (Mbps)')
     axes[1].set_ylabel('Latency (ms)')
     axes[1].set_xlabel('Time', labelpad=15)
@@ -43,23 +48,89 @@ def generateGraphs(data: pd.DataFrame):
     fig = plt.gcf()
     fig.autofmt_xdate(rotation=45)
     fig.subplots_adjust(bottom=0.25, left=0.25)
-    plt.show()
 
-def analyzeData():
-    data = pd.read_csv(RESULTS_PATH.resolve())
+    imagePath = SCRIPT_PATH / 'results.png'
+    plt.savefig(imagePath)
 
-    generateGraphs(data)
+    return imagePath
 
-    print(data.mean())
+@dataclass
+class SpeedData:
+    averageDownloadBandwidth: float
+    averageUploadBandwidth: float
+    averagePing: float
+    startDate: datetime
+    endDate: datetime
+    plotFile: Path
+
+def analyzeData(filename: Path) -> pd.Series:
+    data = pd.read_csv(filename)
+
+    plotFile = generateGraphs(data)
+
+    averages = data[['down', 'up', 'ping']].mean()
+
+    dateFormat = '%Y-%m-%dT%H:%M:%SZ'
+    startDate = datetime.strptime(data['time'].iloc[0], dateFormat)
+    endDate = datetime.strptime(data['time'].iloc[-1], dateFormat)
+
+    return SpeedData(
+        averages['down'],
+        averages['up'],
+        averages['ping'],
+        startDate,
+        endDate,
+        plotFile,
+    )
+
+def notify(data: SpeedData):
+    startDate = data.startDate.date()
+    endDate = data.endDate.date()
+
+    subject = f'Internet Speed Summary for {startDate}'
+    subject += '' if startDate == endDate else f' to {endDate}'
+
+    sendEmail(
+        to='beechmicah@gmail.com',
+        subject=subject,
+        body=f'''
+        <table>
+            <tbody><tr>
+                <td>Average download speed:</td>
+                <td>{data.averageDownloadBandwidth:.2f} Mbps</td>
+            </tr>
+            <tr>
+                <td>Average upload speed:</td>
+                <td>{data.averageUploadBandwidth:.2f} Mbps</td>
+            </tr>
+            <tr>
+                <td>Average ping:</td>
+                <td>{data.averagePing:.2f} ms</td>
+            </tr>
+            </tbody>
+        </table>
+
+        <br/>
+        ''',
+        bodyType='html',
+        attachments=[data.plotFile],
+    )
+
+    data.plotFile.unlink()
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('command', choices=['test', 'graph'])
+    parser.add_argument('command', choices=['test', 'analyze', 'email'])
 
     args = parser.parse_args()
 
     match args.command:
         case 'test':
-            runSpeedtest()
+            runSpeedtest(RESULTS_CSV_PATH)
         case 'analyze':
-            analyzeData()
+            results = analyzeData(RESULTS_CSV_PATH)
+            print(results)
+            print(type(results.startDate))
+        case 'email':
+            results = analyzeData(RESULTS_CSV_PATH)
+            notify(results)
